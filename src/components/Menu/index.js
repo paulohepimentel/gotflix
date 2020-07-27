da33afd48c783314a9d46048da2ec20de3986e4f
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-07-27 14:16:14 -0300

    Estruturação inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/images/logo.png';
+import './Menu.css';
+//import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="phlix logo"/>
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Video
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
