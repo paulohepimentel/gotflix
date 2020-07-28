ac77fc187185c57975a4af017f016e27ba113e9b
Paulo Henrique <paulohenrique.pimentel@gmail.com>
2020-07-28 17:04:26 -0300

    Prototipação inicial pag404

--- src/pages/Pagina404/index.js
+++ src/pages/Pagina404/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import PageDefault from '../../components/PageDefault';
 
 function Pagina404 () {
     return (
-        <div class="wrapper">
-            <h1>Play Snake! (using arrows)</h1>
-            
-            <canvas id="snakeGame"></canvas>
-            
-            <h2 id="snakeGameScore">Score: 0</h2>
-            <h3 id="snakeGameHighestScore">Highest Score: 0</h3>
-        </div>
+        <PageDefault>
+            <div>
+                <h1>Opa</h1>
+                <h2>Essa página não existe</h2>
+            </div>
+        </PageDefault>
     );
 }
 
